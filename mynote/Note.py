33ecebefab5625716236ed2145fabdda97d2d249
kidnapmydog <kidnapmydog@163.com>
2021-04-20 11:21:33 +0800

    Default Changelist

--- mynote/Note.py
+++ mynote/Note.py
@@ -7,6 +7,7 @@ class Note(object):
     def __init__(self, memo, tag):
         self.memo = memo
         self.tag = tag
+        self.create_date = datetime.datetime.today()
         global last_id
         last_id += 1
         self.id = last_id
@@ -18,20 +19,20 @@ class NoteBook(object):
     def __init__(self):
         self.notes = []
 
+    def __find_note(self, note_id):
+        for note in self.notes:
+            if note.id == note_id:
+                return note
+        return None
+
     def new_note(self, memo, tags=""):
         self.notes.append(Note(memo, tags))
 
     def modify_memo(self, note_id, memo):
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        self.__find_note(note_id).memo = memo
 
     def modify_tags(self, note_id,tags):
-        for note in self.notes:
-            if note.id == note_id:
-                note.tag = tags
-                break
+        self.__find_note(note_id).tag = tags
 
     def search(self, filter):
         return [note for note in self.notes if note.match(filter)]
