f5cf180d69a71dadb52d7c2d68097d67a27f9a33
kidnapmydog <kidnapmydog@163.com>
2021-05-17 10:59:03 +0800

    test commit

--- MyTools/File_Oper_Tools/FileOper.py
+++ MyTools/File_Oper_Tools/FileOper.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import pathlib
 import filecmp
+import PIL
 
 
 def getFiles(extendions,src_dir):
@@ -25,16 +26,50 @@ def classifyFiles(src_dir, dest_dir,exts):
             if not (dest_dir / i.name).exists():
                 i.replace(dest_dir / suffixName / i.name)
 
+def replaceDuplicatFiles(flag, src_dir, dest_dir=None):
+    if flag == 0 and dest_dir is None:
+        return False
+
+    try:
+        src_dir = src_dir
+        if not src_dir.exists():
+            raise Exception(f"The {src_dir} is not exists.")
+        if not src_dir.is_dir():
+            raise Exception(f"The {src_dir} is not directory.")
+        dest_dir = dest_dir
+        result = list(src_dir.glob('*'))
+        file_list = []
+        for i in result:
+            if i.is_file():
+                file_list.append(i)
+        for m in file_list:
+            for n in file_list:
+                if m != n and m.exists() and n.exists():
+                    if filecmp.cmp(m,n):
+                        if flag == 0:
+                            if not dest_dir.exists():
+                                dest_dir.mkdir(parents=True)
+                            if not (dest_dir/n.name).exists():
+                                print(dest_dir/n.name)
+                                n.replace(dest_dir / n.name)
+                        else:
+                            n.unlink()
+    except Exception as e:
+        print("The program has something wrong:",e)
+
 
 
 '''This is test code'''
 
 if __name__ == '__main__':
-    print("This is a file classify function")
-    src_dir = pathlib.Path(input("the src_dir:"))
-    dest_dir = pathlib.Path(input("the dest_dir:"))
-    exts = input("The extendions")
-    classifyFiles(src_dir, dest_dir, exts)
+    # print("This is a file classify function")
+    # src_dir = pathlib.Path(input("the src_dir:"))
+    # dest_dir = pathlib.Path(input("the dest_dir:"))
+    # exts = input("The extendions")
+    # classifyFiles(src_dir, dest_dir, exts)
+    src_dir = pathlib.Path(r'/Users/quyx/Documents/Code/Python/TestPY_Project/src/')
+    dest_dir = pathlib.Path(r'/Users/quyx/Documents/Code/Python/TestPY_Project/dest/')
+    replaceDuplicatFiles(1,src_dir,dest_dir)
 
 
 
